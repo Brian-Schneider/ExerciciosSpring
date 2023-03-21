12a06790f89c9e68db588ce2f4098b359c077688
Brian Schneider de Lima <brian.schneider.epica@gmail.com>
2023-03-21 19:23:06 -0300

    Exercício CRUD Loja de Games

---
+++ lojaGames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,93 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome do produto é OBRIGATÓRIO!")
+	@Size(min = 5, max = 100, message = "O nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nomeProd;
+	
+	@NotNull(message = "O preço do produto é OBRIGATÓRIO!")
+	private BigDecimal preco;
+	
+	@Size(min = 5, max = 20, message = "A categoria deve ter no mínimo 05 e no máximo 50 caracteres")
+	private String plataforma;
+
+	@NotBlank(message = "O fabricante e/ou distribuidora é OBRIGATÓRIO!")
+	@Size(min = 5, max = 50, message = "O fabricante e/ou distribuidora deve ter no mínimo 05 e no máximo 50 caracteres")
+	private String fabrDist;
+	
+	@NotNull(message = "A quantidade em estoque é OBRIGATÓRIA!")
+	private Integer estoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeProd() {
+		return nomeProd;
+	}
+	public void setNomeProd(String nomeProd) {
+		this.nomeProd = nomeProd;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	public Integer getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+	
+	public String getPlataforma() {
+		return plataforma;
+	}
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	public String getFabrDist() {
+		return fabrDist;
+	}
+	public void setFabrDist(String fabrDist) {
+		this.fabrDist = fabrDist;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+
+}
