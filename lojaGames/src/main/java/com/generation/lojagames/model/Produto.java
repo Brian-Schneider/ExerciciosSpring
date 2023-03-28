19de9561a8301e42abaa0f49f80ca3fccfa48eb7
Brian Schneider de Lima <brian.schneider.epica@gmail.com>
2023-03-27 21:59:06 -0300

    Criados Usuário e Camadas de Security e Service para o exercício lojaGames

--- lojaGames/src/main/java/com/generation/lojagames/model/Produto.java
+++ lojaGames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -43,6 +43,10 @@ public class Produto {
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+	
 	public Long getId() {
 		return id;
 	}
@@ -85,6 +89,12 @@ public class Produto {
 	}
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 	
